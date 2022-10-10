89ac875ddc446e4ee9b02815ac9d46547cbdd1bc
Amy <amyannnwong@gmail.com>
2022-10-10 15:20:35 -0400

    created browser routed and configured first route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+// entry point
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
