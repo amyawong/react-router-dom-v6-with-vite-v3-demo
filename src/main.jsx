8830c520a35e6f77199efb3a67f2d79693afcc99
Amy <amyannnwong@gmail.com>
2022-10-10 15:31:52 -0400

    root component

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,27 @@
-// entry point
+// entry point file
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
+// 2c. via src/routes/root.jsx
+// 2d. update root route's element
+import Root from "./routes/root";
+
+// 1a. create browser router and configure first route
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Root />,
   },
 ]);
 
+// 1b. pass in router into <RouterProvider />
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// 2a. go to src/routes/root.jsx
---
+++ src/routes/root.jsx
@@ -0,0 +1,43 @@
+// 2a. via src/main.jsx
+// 2b. create the root layout component
+  // first route is what often called the "root route" since the rest of routes will render inside of it. 
+  // it will serve as the root layout of the UI, and will have nested layouts as it gets farther along.
+
+export default function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+    </>
+  );
+}
+
+// 2c. Go to src/main.jsx
